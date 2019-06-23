91b3173ba5227bd058948868c94f79a9042622f3
Chris Cardelle <chris.cardelle@gmail.com>
2019-06-23 13:57:35 -0400

    Navbar Implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar.Brand href="#home">Cardelle</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Main</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#link">About Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Main />
+      </Container>
+
+      {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Under Construction</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +35,7 @@ function App() {
           Learn React
         </a>
       </header>
+    </div> */}
     </div>
   );
 }
